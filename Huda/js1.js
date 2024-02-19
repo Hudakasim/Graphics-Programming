f55592f605a7521d05431a6a23513c4ff7cd07d6
Huda Kasımoğlu <141566506+Hudakasim@users.noreply.github.com>
2024-02-19 12:11:05 +0300

    Add files via upload

---
+++ Huda/js1.js
@@ -0,0 +1,29 @@
+// print
+console.log('hello world');
+
+// basic operations
+let a = 5;
+let b = 10;
+let c = a + b;
+console.log(c);
+
+// if statment
+if (c > 10)
+{
+	c = c + 100;
+}
+else
+{
+	c = c - 10;
+}
+console.log(c);
+
+
+// functions
+function subtract(num1, num2)
+{
+var num3 = num1 - num2;
+console.log(num3);
+}
+
+subtract (100, 50);
